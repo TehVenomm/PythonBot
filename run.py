4acb8c03245bb1f10d44fa0658aee12fc02a4bf2
Gabriel Braz e Santos <gabriel.braz@nwaycard.com.br>
2019-04-01 11:01:33 -0300

    Test

--- run.py
+++ run.py
@@ -13,91 +13,11 @@ async def on_ready():
     print('------')
 
 @bot.command()
-async def behemoth(ctx, *a):
-    i = 0
-    joinedInput = " ".join(a)
-    filteredInput = myUtilities.filterInput(joinedInput)
-    inputWordArray = filteredInput.split()
+async def roleonline(ctx, *a):
+	role = discord.utils.get(ctx.guild.roles, name="A")
+    for user in ctx.guild.members:
+        if user.status != discord.Status.offline:
+			await user.add_roles(role)
 
-    attributesArray = myUtilities.matchWeaponAttributes(inputWordArray)
-    for key, content in attributesArray.items():
-        if (content):
-            i+=1
-
-    if (i > 1):
-        queryResults = myUtilities.fetchBehemothByTypeDB(attributesArray)
-    else:
-        queryResults = myUtilities.fetchBehemothDB(filteredInput)
-
-    if (len(queryResults) > 0 and len(queryResults) <= 10):
-        embed = myUtilities.behemothEmbedGenerator(queryResults, filteredInput)
-        await ctx.send(embed=embed)
-    else:
-        await ctx.send("Behemoth not found.")
-
-
-@bot.command()
-async def weapon(ctx, *a):
-    joinedInput = " ".join(a)
-    filteredInput = myUtilities.filterInput(joinedInput)
-    queryResults = myUtilities.fetchWeaponDB(filteredInput)
-
-    if (len(queryResults) == 1):
-        embed = myUtilities.weaponEmbed(queryResults)
-        await ctx.send(embed=embed)
-    else:
-        await ctx.send("Behemoth not found.")
-
-@bot.command()
-async def armor(ctx, *a):
-    joinedInput = " ".join(a)
-    filteredInput = myUtilities.filterInput(joinedInput)
-    queryResults = myUtilities.fetchArmorDB(filteredInput)
-
-    if (len(queryResults) == 4):
-        embed = myUtilities.armorEmbed(queryResults)
-        await ctx.send(embed=embed)
-    else:
-        await ctx.send("Behemoth not found.")
-
-@bot.command()
-async def magi(ctx, *a):
-    joined = " ".join(a)
-    filteredInput = myUtilities.filterInput(joined)
-    queryResults = myUtilities.fetchMagiDB(filteredInput)
-
-    if (len(queryResults) > 0 and len(queryResults) <= 10):
-        embed = myUtilities.magiEmbedGenerator(queryResults, filteredInput)
-        await ctx.send(embed=embed)
-    else:
-        await ctx.send("Magi not found.")
-    
-@armor.error
-async def armor_on_error(ctx, error):
-    if isinstance(error, commands.MissingRequiredArgument):
-        embed = discord.Embed(title="The correct syntax is:", colour=discord.Colour(0xff0000), description="`?armor [Behemoth's name]`")
-        await ctx.send(embed=embed)
-    raise error
-
-@weapon.error
-async def weapon_on_error(ctx, error):
-    if isinstance(error, commands.MissingRequiredArgument):
-        embed = discord.Embed(title="The correct syntax is:", colour=discord.Colour(0xff0000), description="`?weapon [Behemoth's name]`")
-        await ctx.send(embed=embed)
-    raise error
-
-@behemoth.error
-async def behemoth_on_error(ctx, error):
-    if isinstance(error, commands.MissingRequiredArgument):
-        embed = discord.Embed(title="The correct syntax is:", colour=discord.Colour(0xff0000), description="`?behemoth [Behemoth's name]`")
-        await ctx.send(embed=embed)
-    raise error
-
-@magi.error
-async def magi_on_error(ctx, error):
-    if isinstance(error, commands.MissingRequiredArgument):
-        embed = discord.Embed(title="The correct syntax is:", colour=discord.Colour(0xff0000), description="`?magi [Magi's name]`")
-        await ctx.send(embed=embed)
-    raise error
 
 bot.run(secrets.getToken())
