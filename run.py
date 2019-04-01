52ed963470e748f4a71b9d0209f92fb6c0fc35b0
Gabriel Braz e Santos <gabriel.braz@nwaycard.com.br>
2019-04-01 13:33:50 -0300

    Removed fat snip snip

--- run.py
+++ run.py
@@ -1,6 +1,5 @@
 import discord
 from discord.ext import commands
-import myUtilities
 import secrets
 
 bot = commands.Bot(command_prefix='?')
--- secrets.py
+++ secrets.py
@@ -1,22 +1,6 @@
-import pymysql.cursors
-import pymysql
-import re
 import os
     
 access_token = os.environ["ACCESS_TOKEN"]
-db_name      = os.environ["DB_NAME"]
-db_password  = os.environ["DB_PASSWORD"]
-db_username = os.environ["DB_USERNAME"]
-db_hostname     = os.environ["DB_HOSTNAME"]
-
-def getConnection():
-    connection = pymysql.connect(host=db_hostname,
-                                user=db_username,
-                                password=db_password,
-                                db=db_name,
-                                charset='utf8mb4',
-                                cursorclass=pymysql.cursors.DictCursor)
-    return connection
 
 def getToken():
     token = access_token
