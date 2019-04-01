5ea5200874a78462a438ce893ed27dfad22db90c
Gabriel Braz e Santos <gabriel.braz@nwaycard.com.br>
2019-04-01 11:06:21 -0300

    Indentation fix

--- run.py
+++ run.py
@@ -7,17 +7,16 @@ bot = commands.Bot(command_prefix='?')
 
 @bot.event
 async def on_ready():
-    print('Logged in as')
-    print(bot.user.name)
-    print(bot.user.id)
-    print('------')
+	print('Logged in as')
+	print(bot.user.name)
+	print(bot.user.id)
+	print('------')
 
 @bot.command()
 async def roleonline(ctx, *a):
 	role = discord.utils.get(ctx.guild.roles, name="A")
-    for user in ctx.guild.members:
-        if user.status != discord.Status.offline:
+	for user in ctx.guild.members:
+		if user.status != discord.Status.offline:
 			await user.add_roles(role)
-
 
 bot.run(secrets.getToken())
